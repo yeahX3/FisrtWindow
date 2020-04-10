be00f8027b182a7c459e11f35a825817ead99c91
Nelson <nelson_dg84@hotmail.com>
2020-04-10 04:52:40 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnMessage = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtSum1 = New System.Windows.Forms.TextBox()
+        Me.txtSum2 = New System.Windows.Forms.TextBox()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.txtSum3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(13, 13)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(35, 13)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Name"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(55, 13)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(16, 60)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 2
+        Me.btnOK.Text = "&OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnMessage
+        '
+        Me.btnMessage.Location = New System.Drawing.Point(122, 60)
+        Me.btnMessage.Name = "btnMessage"
+        Me.btnMessage.Size = New System.Drawing.Size(75, 23)
+        Me.btnMessage.TabIndex = 3
+        Me.btnMessage.Text = "&Message"
+        Me.btnMessage.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(234, 60)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtSum1
+        '
+        Me.txtSum1.Location = New System.Drawing.Point(16, 128)
+        Me.txtSum1.Name = "txtSum1"
+        Me.txtSum1.Size = New System.Drawing.Size(56, 20)
+        Me.txtSum1.TabIndex = 5
+        '
+        'txtSum2
+        '
+        Me.txtSum2.Location = New System.Drawing.Point(135, 128)
+        Me.txtSum2.Name = "txtSum2"
+        Me.txtSum2.Size = New System.Drawing.Size(56, 20)
+        Me.txtSum2.TabIndex = 6
+        '
+        'btnPlus
+        '
+        Me.btnPlus.Location = New System.Drawing.Point(80, 126)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(23, 23)
+        Me.btnPlus.TabIndex = 7
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = True
+        '
+        'txtSum3
+        '
+        Me.txtSum3.Location = New System.Drawing.Point(269, 129)
+        Me.txtSum3.Name = "txtSum3"
+        Me.txtSum3.Size = New System.Drawing.Size(56, 20)
+        Me.txtSum3.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtSum3)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.txtSum2)
+        Me.Controls.Add(Me.txtSum1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnMessage)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnMessage As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtSum1 As TextBox
+    Friend WithEvents txtSum2 As TextBox
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents txtSum3 As TextBox
+End Class
